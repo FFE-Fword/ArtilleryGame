d1c2bc02ef3fb638cd14e24897ecf5825e4e5aea
Dkass <d.kassaye@outlook.com>
2019-04-23 13:38:13 -0500

    cleaned

--- enemies.py
+++ enemies.py
@@ -4,8 +4,7 @@ class Enemy():
     def __init__(self):
         self.hp = 100
         self.armour = 100
-        self.description = "An enemy of Canada that must not\
-        be allowed to live."
+        self.description = "An enemy of Canada that must not be allowed to live."
 
     def is_alive(self):
         return self.hp > 0
@@ -15,8 +14,7 @@ class Armour(Enemy):
     def __init__(self):
         self.hp *= 1.25
         self.armour *= 1
-        self.description = "The enemy apears to be hatches down in\
-        some sort of armoured vehicle."
+        self.description = "The enemy apears to be hatches down in some sort of armoured vehicle."
 
     def lose_armour(self, damage):
         self.armour -= damage
@@ -26,8 +24,7 @@ class Armour(Enemy):
 class AntiAir(Enemy):
     def __init__(self):
         self.armour *= 0.5
-        self.description = "It's a enemy air defence vehicle! This\
-        should be top priority!"
+        self.description = "It's a enemy air defence vehicle! This should be top priority!"
 
 
 class Dissmounts(Enemy):
@@ -40,11 +37,11 @@ class Dissmounts(Enemy):
 class MBT(Armour):
     def __init__(self):
         self.armour *= 1.25
-        self.description = "Enemy MBT out of concealment! Prime target for the\
-         guns."
+        self.description = "Enemy MBT out of concealment! Prime target for artillery"
+         
 
 
 class Recce(Armour):
     def __init__(self):
         self.armour *= 0.75
-        self.description = "Light and fast, enemy recce vehicle can't take a\ hit. But also won't stick around long"
+        self.description = "Light and fast. Recce vehicles can't take a hit."
