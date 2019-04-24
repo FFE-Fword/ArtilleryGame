5fd2dedee98facbad98c13b02de2099f9f61a50a
Dkass <d.kassaye@outlook.com>
2019-04-23 21:55:22 -0500

    got the classes to work... not fully tested tho

--- enemies.py
+++ enemies.py
@@ -1,41 +1,43 @@
 
 
-""" CLASSES DO NOT WORK YET!!!!!Contains all enemy classes. They all have base      variables of hp (health), armour and a base description. When enemies take      damage, their armour will deplete until 0, then hp will deplete."""
+""" CLASSES work with basic functionality. Contains all enemy classes. They all have base variables of hp (health), armour and a base description. When enemies take damage, their armour will deplete until 0, then hp will deplete."""
 
 
 class Enemy():  # All enemies a derived from this class
 
-    def __init__(self):
-        self.hp = 100
-        self.armour = 100
-        self.description = "An enemy of Canada that must not be allowed ",\
-            "to live!!"
+    base_hp = 100
+    base_armour = 100
+    description = ("An enemy of Canada that must not be allowed"
+    " to live!!!")
+
+    def __str__(self):
+        return "{}".format(self.description)
 
     # Checks if enemy health is above 0
 
     def is_alive(self):
-        return self.hp > 0
+        return self.base_hp > 0
 
-    # Takes in amount(amt) as an int and adds to self.armour
+    # Takes in amount(amt) as an int and adds to    self.base_armour
 
     def add_armour(self, amt):
-        self.armour += amt
+        self.base_armour += amt
 
     # Function that detucts taken damage from armour
 
     def lose_armour(self, damage):
-        self.armour -= damage
-        return self.armour
+        self.base_armour -= damage
+        return self.base_armour
 
 
 # Armour, AntiAir and Dissmounts direct child of Enemy()
 
 class Armour(Enemy):
-    def __init__(self):
-        self.hp *= 1.25
-        self.armour *= 1
-        self.description = "The enemy apears to be hatches down in some sort",\
-            " of armoured vehicle"
+       def __init__(self):
+            super().__init__() 
+            self.base_hp *= 1.25
+            self.base_armour *= 1
+            self.description = ("The enemy apears to be hatches down in some" "sort of armoured vehicle")
 
 
 """ TODO: add functions to dissmounts to add more armour if they have some
@@ -45,10 +47,12 @@ class Armour(Enemy):
 
 class Dissmounts(Enemy):  # No armour unless under cover.
     def __init__(self, cover=None):
-        self.hp *= 0.5
-        self.armour = 0
-        self.description = "Enemy soldiers. Basic grunts. Very vulnerable",\
-            " in the open but harder to kill once dug-in."
+        super().__init__()
+        self.cover = cover
+        self.base_hp *= 0.5
+        self.base_armour = 0
+        self.description = ("Enemy soldiers. Basic grunts. Very vulnerable"
+            " in the open but harder to kill once dug-in.")
 
 
 """ These 3 are direct from Armour(). Armour is being multiplied by a set
@@ -57,20 +61,24 @@ class Dissmounts(Enemy):  # No armour unless under cover.
 
 class AntiAir(Armour):
     def __init__(self):
-        self.armour *= 0.5
-        self.description = "It's a enemy air defence vehicle! This should ", \
-            "be a top priority kill."
+        super().__init__()
+        self.base_armour *= 0.5
+        self.description = ("It's a enemy air defence vehicle! This should "
+            "be a top priority kill.")
 
 
 class MBT(Armour):
     def __init__(self):
-        self.armour *= 1.25
-        self.description = "Enemy MBT out of concealment! Prime target for ",\
-            "the Guns!"
+        super().__init__()
+        self.base_armour *= 1.25
+        self.description = ("Enemy MBT out of concealment! Prime target for "
+            "the Guns!")
 
 
 class Recce(Armour):
     def __init__(self):
-        self.armour *= 0.75
-        self.description = "Light and fast, recce vehicles aren't ",\
-            "made to take a hit."
+        super().__init__()
+        self.base_armour *= 0.75
+        self.description = ("Light and fast, recce vehicles aren't "
+            "made to take a hit.")
+
--- testclasses.py
+++ testclasses.py
@@ -1,20 +1,32 @@
+# Getting Class inheritance to work properly
+
 class Enemy():  # All enemies a derived from this class
+    
+    base_hp = 100
+    base_armour = 100
+    description = ("An enemy of Canada that must not be allowed"
+    " to live!!!")
 
-    def __init__(self,hp=100,armour=100,description=""):
-        self.hp = hp
-        self.armour = armour
-        self.description = "An enemy of Canada that must not be allowed ",\
-            "to live!!"
-
+    def __str__(self):
+        return "{}".format(self.description)  
+        
 class Armour(Enemy):
        def __init__(self):
-               super().__init__(self.hp,self.armour,self.description) 
-                         
-       
+               super().__init__() 
+               self.base_hp *= 1.25
+        
+class MBT(Armour):
+    def __init__(self):
+        super().__init__()
+        self.base_armour *= 1.25
+        self.description = ("Enemy MBT out of concealment! Prime target for"
+            "the Guns!")
+
 def check():
-        tsevtwo = Armour()
-        tsevtwo.armour
-        print(tsevtwo.armour)
+        tsevtwo = MBT()
+        print(tsevtwo.base_armour)
+        print(tsevtwo.base_hp)
+        print(tsevtwo)
         return False
 
 check()
