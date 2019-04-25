646298363493f0bf986f30e7f28595191dda7720
Dkass <d.kassaye@outlook.com>
2019-04-25 15:02:26 -0500

    working on en gen

--- spawn.py
+++ spawn.py
@@ -3,20 +3,19 @@ import npc
 
 # Setting enemy formation size in military breakdown style, a(Armoured) or i(Infantry); sec == section, plt == platoon, coy == company
 
-a_pltplus = False
-i_pltplus = False
-a_trpplus = False
-i_secplus = False
+
 a_coy = False
 i_coy = False
-a_plt = False
-i_plt = False
-i_sec = False
 a_coyminus = False
 i_coyminus = False
+a_pltplus = False
+i_pltplus = False
+a_plt = False
+i_plt = False
 a_pltminus = False
 i_pltminus = False
-a_trpminus = False
+i_secplus = False
+i_sec = False
 i_secminus = False
 a_1x = False
 i_1x =False
@@ -69,101 +68,135 @@ def spawn_anti():
 
 # Use .count to find occurance in list and changes enemy sizes to True if they 
 # meet the requirements. Works, could use some cleaning.
-
-
-# Working like expected. Take another look.
-
-def find_diss_size(list):
+# NOT WORKING AS EXPECTED, 
+def find_en_size(list):
         spawn_en()
-        if list.count(en_diss) == 128:
-                i_coy = True
-                print("It's a coy")
-        elif list.count(en_diss) > 50 and list.count(en_diss) <= 127:
-                i_coyminus = True
-                print("It's a coy minus")
-        elif list.count(en_diss) <= 49  and list.count(en_diss) >= 33:
-                i_pltplus = True
-                print("It's a plt plus")
-        elif list.count(en_diss) == 32:
-                i_plt = True
-                print("It's a plt")
-        elif list.count(en_diss) <=31 and list.count(en_diss) >= 16:
-                i_pltminus = True
-                print("It's a plt minus")
-        elif list.count(en_diss) <=15 and list.count(en_diss) >= 9:
-                i_secplus = True
-                print("It's a sec plus")
-        elif list.count(en_diss) == 8:
-                i_sec = True
-                print("It's a sec")
-        elif list.count(en_diss) <=7 and list.count(en_diss) >=1:
-                i_secminus = True
-                print("It's a sec")
+        if (en_diss and en_arm and en_recce) in enemy: # If diismounts, arm and 
+                # recce are generated
+                print("Enemy dissmounts sup by armour and armoured recce")
+                if (list.count(en_diss) <=15 and list.count(en_diss) >= 9) and (list.count(en_arm) == 3) and (list.count(en_recce) == 3):
+                        i_secplus, a_plt = True, True
+                        print("It's a sec+ of diss sup by plt of arm and plt of recce.")
+                elif (list.count(en_diss) <=15 and list.count(en_diss) >= 9) and (list.count(en_arm) == 2)and (list.count(en_recce) == 2):
+                        i_secplus, a_pltminus = True, True
+                        print("It's a sec+ of diss sup by plt- of arm and plt- of recce.")
+                elif (list.count(en_diss) <=15 and list.count(en_diss) >= 9) and (list.count(en_arm) == 1) and (list.count(en_recce) == 1):
+                        i_secplus, a_1x = True, True
+                        print("It's a sec+ of diss sup by 1x arm and 1x recce.")
+                elif list.count(en_diss) == 8 and (list.count(en_arm) == 3) and (list.count(en_recce) == 3):
+                        i_sec, a_plt = True, True
+                        print("It's a sec of diss sup by plt of arm and plt of recce.")
+                elif (list.count(en_diss) == 8) and (list.count(en_arm) == 2)and (list.count(en_recce) == 2):
+                        i_sec, a_pltminus = True, True
+                        print("It's a sec of diss sup by plt- of arm and plt- of recce.")
+                elif (list.count(en_diss) == 8) and (list.count(en_arm) == 1) and (list.count(en_recce) == 1):
+                        i_sec, a_1x = True, True
+                        print("It's a sec of diss sup by 1x arm and 1x recce.")
+                elif (list.count(en_diss) <=7 and list.count(en_diss) >=1) and (list.count(en_arm) == 3) and (list.count(en_recce) == 3):
+                        i_secminus, a_plt = True, True
+                        print("It's a sec- of diss sup by plt of arm and plt of recce.")
+                elif (list.count(en_diss) <=7 and list.count(en_diss) >=1) and (list.count(en_arm) == 2)and (list.count(en_recce) == 2):
+                        i_secminus, a_pltminus = True, True
+                        print("It's a sec- of diss sup by plt- of arm and plt- of recce.")
+                elif (list.count(en_diss) <=7 and list.count(en_diss) >=1) and (list.count(en_arm) == 1) and (list.count(en_recce) == 1):
+                        i_secminus, a_1x = True, True
+                        print("It's a sec- of diss sup by 1x arm and 1x recce.")
+                
+        elif en_diss and en_recce in enemy: # dissmounts and recce only
+                if (list.count(en_diss) <=15 and list.count(en_diss) >= 9) and  (list.count(en_recce) == 3):
+                        i_secplus, a_plt = True, True
+                        print("It's a sec+ of diss sup by a plt of recce.")
+                elif (list.count(en_diss) <=15 and list.count(en_diss) >= 9) and (list.count(en_recce) == 2):
+                        i_secplus, a_pltminus = True, True
+                        print("It's a sec+ of diss sup by a plt- of recce.")
+                elif (list.count(en_diss) <=15 and list.count(en_diss) >= 9) and (list.count(en_recce) == 1):
+                        i_secplus, a_1x = True, True
+                        print("It's a sec+ of diss sup by 1x recce.")
+                elif list.count(en_diss) == 8 and (list.count(en_recce) == 3):
+                        i_sec, a_plt = True, True
+                        print("It's a sec of diss sup by plt of recce.")
+                elif (list.count(en_diss) == 8) and (list.count(en_recce) == 2):
+                        i_sec, a_pltminus = True, True
+                        print("It's a sec of diss sup by plt- of recce.")
+                elif (list.count(en_diss) == 8) and (list.count(en_recce) == 1):
+                        i_sec, a_1x = True, True
+                        print("It's a sec of diss sup by 1x recce.")
+                elif (list.count(en_diss) <=7 and list.count(en_diss) >=1) and (list.count(en_recce) == 3):
+                        i_secminus, a_plt = True, True
+                        print("It's a sec- of diss sup by plt of recce.")
+                elif (list.count(en_diss) <=7 and list.count(en_diss) >=1) and(list.count(en_recce) == 2):
+                        i_secminus, a_pltminus = True, True
+                        print("It's a sec- of diss sup by plt- of recce.")
+                elif (list.count(en_diss) <=7 and list.count(en_diss) >=1) and (list.count(en_recce) == 1):
+                        i_secminus, a_1x = True, True
+                        print("It's a sec- of diss sup by 1x recce.")
+
+        elif en_diss and en_arm in enemy: # dissmounts and arm only
+                if (list.count(en_diss) <=15 and list.count(en_diss) >= 9) and  (list.count(en_arm) == 3):
+                        i_secplus, a_plt = True, True
+                        print("It's a sec+ of diss sup by a plt of armour.")
+                elif (list.count(en_diss) <=15 and list.count(en_diss) >= 9) and (list.count(en_arm) == 2):
+                        i_secplus, a_pltminus = True, True
+                        print("It's a sec+ of diss sup by a plt- of armour.")
+                elif (list.count(en_diss) <=15 and list.count(en_diss) >= 9) and (list.count(en_arm) == 1):
+                        i_secplus, a_1x = True, True
+                        print("It's a sec+ of diss sup by 1x armour.")
+                elif list.count(en_diss) == 8 and (list.count(en_arm) == 3):
+                        i_sec, a_plt = True, True
+                        print("It's a sec of diss sup by plt of armour.")
+                elif (list.count(en_diss) == 8) and (list.count(en_arm) == 2):
+                        i_sec, a_pltminus = True, True
+                        print("It's a sec of diss sup by plt- of armour.")
+                elif (list.count(en_diss) == 8) and (list.count(en_arm) == 1):
+                        i_sec, a_1x = True, True
+                        print("It's a sec of diss sup by 1x armour.")
+                elif (list.count(en_diss) <=7 and list.count(en_diss) >=1) and (list.count(en_arm) == 3):
+                        i_secminus, a_plt = True, True
+                        print("It's a sec- of diss sup by plt of armour.")
+                elif (list.count(en_diss) <=7 and list.count(en_diss) >=1) and(list.count(en_arm) == 2):
+                        i_secminus, a_pltminus = True, True
+                        print("It's a sec- of diss sup by plt- of armour.")
+                elif (list.count(en_diss) <=7 and list.count(en_diss) >=1) and (list.count(en_arm) == 1):
+                        i_secminus, a_1x = True, True
+                        print("It's a sec- of diss sup by 1x armour.")
+
+
+        elif en_arm and en_recce in enemy: # recce and arm only
+                if (list.count(en_recce) == 3) and  (list.count(en_arm) == 3):
+                        a_plt = True
+                        print("It's a plt of recce sup by a plt of armour.")
+                elif (list.count(en_recce) == 3) and  (list.count(en_arm) == 2):
+                        a_plt, a_pltminus = True, True
+                        print("It's a plt of recce sup by a plt- of armour.")
+                elif (list.count(en_recce) == 3) and  (list.count(en_arm) == 1):
+                        a_plt, a_1x = True, True
+                        print("It's a plt of recce sup by 1x armour.")
+                elif list.count(en_recce) == 2 and (list.count(en_arm) == 3):
+                        a_pltminus, a_plt = True, True
+                        print("It's a plt- of recce sup by plt of armour.")
+                elif (list.count(en_recce) == 2) and (list.count(en_arm) == 2):
+                        a_pltminus = True
+                        print("It's a plt- of recce sup by plt- of armour.")
+                elif (list.count(en_recce) == 2) and (list.count(en_arm) == 1):
+                        a_pltminus, a_1x = True, True
+                        print("It's a plt- of recce sup by 1x armour.")
+                elif (list.count(en_recce) == 1) and (list.count(en_arm) == 3):
+                        a_1x, a_plt = True, True
+                        print("It's a 1x recce sup by plt of armour.")
+                elif (list.count(en_recce) == 1) and (list.count(en_arm) == 2):
+                        a_1x, a_pltminus = True, True
+                        print("It's a 1x recce sup by plt- of armour.")
+                elif (list.count(en_recce) == 1) and (list.count(en_arm) == 1):
+                        a_1x = True
+                        print("It's a 1x recce sup by 1x armour.")
+       
+
         else:
                 print("There are no enemies. Continiue watching your arcs!")
 
 
-# Working like expected. Take another look.
-
-def find_recce_size(list):
-        spawn_recce()  
-        if list.count(en_recce) >=13 and list.count(en_recce) <=16:
-                a_pltplus = True
-                print("It's a coy plus of armoured")
-        elif list.count(en_recce) ==12:
-                a_coy = True
-                print("It's a coy of  armoured recce")
-        elif list.count(en_recce) >= 8 and list.count(en_recce) <=11:
-                a_coyminus = True
-                print("It's a coy minus of armoured recce")
-        elif list.count(en_recce) >=4 and list.count(en_recce) <= 7:
-                a_pltminus = True
-                print("It's a plt plus of armoured recce")
-        elif list.count(en_recce) ==3:
-                a_plt = True
-                print("It's a plt")
-        elif list.count(en_recce) == 2:
-                a_pltminus = True
-                print("It's a plt minus")
-        elif list.count(en_recce) == 1:
-                a_1x = True
-                print("It's 1x recce vehicle")
-        else:
-                print("There are no enemies. Continiue watching your arcs!")
-
-# Working like expected. Take another look.
-
-def find_arm_size(list):
-        spawn_en()        
-        if list.count(en_arm) >=13 and list.count(en_arm) <=16:
-                a_pltplus = True
-                print("It's a coy plus of armour")
-        elif list.count(en_arm) ==12:
-                a_coy = True
-                print("It's a coy of armour")
-        elif list.count(en_arm) >= 8 and list.count(en_arm) <=11:
-                a_coyminus = True
-                print("It's a coy minus of armour")
-        elif list.count(en_arm) >=4 and list.count(en_arm) <= 7:
-                a_pltminus = True
-                print("It's a plt plus of armour")
-        elif list.count(en_arm) ==3:
-                a_plt = True
-                print("It's a plt of armour")
-        elif list.count(en_arm) == 2:
-                a_pltminus = True
-                print("It's a plt minus of armour")
-        elif list.count(en_arm) == 1:
-                a_1x = True
-                print("It's 1x armoured vehicle")
-        else:
-                print("There are no enemies. Continiue watching your arcs!")
 
 
-# Need to make a function that will combine spawed enemeny into groups ie.     # supported by
- 
       
-
-find_diss_size(enemy)
-
+find_en_size(enemy)
 
